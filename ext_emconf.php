8d1f07daf31b49a7d4dcea8f29dd6d1b9fe7ba3d
Jan Maennig <jma@move-elevator.de>
2014-01-06 10:39:04 +0100

    #9657 add ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,31 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'm:e Media Center',
+    'description' => 'Media Center',
+    'category' => 'fe',
+    'author' => 'Jan Maennig',
+    'author_email' => 'jma@move-elevator.de',
+    'author_company' => 'move:elevator',
+    'shy' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'alpha',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '0.0.1',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '6.0.0-6.1.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+);
+?>
