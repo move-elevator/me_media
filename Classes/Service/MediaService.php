a81d4da706dd6ee2646f5a03ae809023e571c5a9
move:elevator (Jan Männig) <jma@move-elevator.de>
2016-02-02 12:18:08 +0100

    MEINTRA-343 div. bugfixes

--- Classes/Service/MediaService.php
+++ Classes/Service/MediaService.php
@@ -21,16 +21,39 @@ class MediaService {
 	 * @param \TYPO3\CMS\Extbase\Mvc\Web\Request $requestData
 	 * @return \MoveElevator\MeMedia\Domain\Model\Media
 	 */
-	public function getRecordByRequestData(Request $requestData) {
-		if (!$requestData->hasArgument('media')) {
+	public function getRecordByRequestDataOrSettings(Request $requestData, $settings) {
+		if (
+			!$requestData->hasArgument('media')
+			&& !$this->checkIsMediaIdInSettings($settings)
+		) {
 			return FALSE;
 		}
 
-		$mediaId = $requestData->getArgument('media');
+		if (!$this->checkIsMediaIdInSettings($settings)) {
+			$mediaId = $requestData->getArgument('media');
+		} else {
+			$mediaId = intval($settings['mediaId']);
+		}
 
 		/** @var \MoveElevator\MeMedia\Domain\Model\Media $media */
 		$media = $this->mediaRepository->findByUid($mediaId);
 
 		return $media;
+	}
+
+	/**
+	 * @param $settings
+	 * @return bool
+	 */
+	protected function checkIsMediaIdInSettings($settings) {
+		if (empty($settings['mediaId']) ) {
+			return FALSE;
+		}
+
+		if (intval($settings['mediaId']) === 0) {
+			return FALSE;
+		}
+
+		return TRUE;
 	}
 }
