9cea243ad1c5d023e85d16011639ae630753e612
move:elevator (Jan Männig) <jma@move-elevator.de>
2016-10-24 10:27:30 +0200

    MEINTRA-754 fixed ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,10 +2,9 @@
 
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'm:e Media Center',
-    'description' => 'Media center to present audio and video file as well as internal streams and stream from' .
-        'external provider youtube, vimeo and my video.',
+    'description' => 'Media center to present audio and video file as well as internal streams and stream from external provider youtube, vimeo and my video.',
     'category' => 'plugin',
-    'author' => 'move : elevator',
+    'author' => 'move:elevator',
     'author_email' => 'typo3@move-elevator.de',
     'author_company' => 'move elevator GmbH',
     'state' => 'stable',
